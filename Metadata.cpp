b7acc0e53668ba5c0a466a1424f3da1773e4647d
Vidar Wahlberg <canidae@amy.(none)>
2008-04-20 00:09:06 +0200

    more fixing on metadata, new ideas coming up

--- Metadata.cpp
+++ Metadata.cpp
@@ -17,48 +17,66 @@ double Metadata::compareMetadata(Metadata target) {
 	/* compare this metadata with target */
 	double score = 0.0;
 	list<string> source = createMetadataList();
-	double match[5][source.size()];
-	for (int a = 0; a < 5; ++a) {
-		for (list<string>::size_type b = 0; b < source.size(); ++b)
-			match[a][b] = 0.0;
-	}
-	int pos;
-	/* album */
-	pos = 0;
-	for (list<string>::iterator s = source.begin(); s != source.end(); ) {
+	double match[4][source.size()];
+	int pos = 0;
+	for (list<string>::iterator s = source.begin(); s != source.end(); ++s) {
 		match[0][pos] = similarity(target.getValue(ALBUM), *s);
+		match[1][pos] = similarity(target.getValue(ARTIST), *s);
+		match[2][pos] = similarity(target.getValue(TITLE), *s);
+		match[3][pos] = (target.getValue(TRACKNUMBER) == *s) ? 1.0 : 0.0;
 		++pos;
-		++s;
 	}
-	/* artist */
-	pos = 0;
-	for (list<string>::iterator s = source.begin(); s != source.end(); ) {
-		match[1][pos] = similarity(target.getValue(ALBUM), *s);
-		++pos;
-		++s;
+	/* find the combination that gives the best score
+	 * don't like this code, it's messy */
+	double best = 0.0;
+	int p[4];
+	int c1 = 0;
+	for (list<string>::iterator s1 = source.begin(); s1 != source.end(); ++s1) {
+		int c2 = 0;
+		for (list<string>::iterator s2 = source.begin(); s2 != source.end(); ++s2) {
+			if (c1 == c2) {
+				++c2;
+				continue;
+			}
+			int c3 = 0;
+			for (list<string>::iterator s3 = source.begin(); s3 != source.end(); ++s3) {
+				if (c1 == c3 || c2 == c3) {
+					++c3;
+					continue;
+				}
+				int c4 = 0;
+				for (list<string>::iterator s4 = source.begin(); s4 != source.end(); ++s4) {
+					if (c1 == c4 || c2 == c4 || c3 == c4) {
+						++c4;
+						continue;
+					}
+					double value = match[0][c1] + match[1][c2] + match[2][c3] + match[3][c4];
+					if (value > best) {
+						best = value;
+						p[0] = c1;
+						p[1] = c2;
+						p[2] = c3;
+						p[3] = c4;
+					}
+					++c4;
+				}
+				++c3;
+			}
+			++c2;
+		}
+		++c1;
 	}
-	/* title */
-	pos = 0;
-	for (list<string>::iterator s = source.begin(); s != source.end(); ) {
-		match[2][pos] = similarity(target.getValue(ALBUM), *s);
-		++pos;
-		++s;
+	if (best > 0.0) {
+		score += match[0][p[0]] * ALBUM_WEIGHT;
+		score += match[1][p[1]] * ARTIST_WEIGHT;
+		score += match[2][p[2]] * TITLE_WEIGHT;
+		score += match[3][p[3]] * TRACKNUMBER_WEIGHT;
 	}
-	/* tracknumber */
-	pos = 0;
-	for (list<string>::iterator s = source.begin(); s != source.end(); ) {
-		//match[3][pos] = similarity(target.getValue(ALBUM), *s);
-		++pos;
-		++s;
+	int durationdiff = abs(target.duration - duration);
+	if (durationdiff < DURATION_LIMIT) {
+		score += (1.0 - durationdiff / DURATION_LIMIT) * DURATION_WEIGHT;
 	}
-	/* duration */
-	pos = 0;
-	for (list<string>::iterator s = source.begin(); s != source.end(); ) {
-		//match[4][pos] = similarity(target.getValue(ALBUM), *s);
-		++pos;
-		++s;
-	}
-	/* TODO */
+	score /= ALBUM_WEIGHT + ARTIST_WEIGHT + TITLE_WEIGHT + TRACKNUMBER_WEIGHT + DURATION_WEIGHT;
 	return score;
 }
 
