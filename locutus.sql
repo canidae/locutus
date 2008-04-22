8973b45db1919d9ad9a07830df7370eb0e6c0faf
Vidar Wahlberg <canidae@amy.(none)>
2008-04-22 20:23:22 +0200

    will need this file too

--- locutus.sql
+++ locutus.sql
@@ -25,7 +25,8 @@ CREATE TABLE album (
     type character varying NOT NULL,
     title character varying NOT NULL,
     released date,
-    asin character varying
+    asin character varying,
+    custom_artist character varying
 );
 
 
@@ -47,38 +48,11 @@ CREATE TABLE artist (
 ALTER TABLE public.artist OWNER TO canidae;
 
 --
--- Name: client; Type: TABLE; Schema: public; Owner: canidae; Tablespace: 
---
-
-CREATE TABLE client (
-    client_id integer NOT NULL,
-    name character varying NOT NULL,
-    password character(32) NOT NULL
-);
-
-
-ALTER TABLE public.client OWNER TO canidae;
-
---
--- Name: client_settings; Type: TABLE; Schema: public; Owner: canidae; Tablespace: 
---
-
-CREATE TABLE client_settings (
-    client_id integer NOT NULL,
-    key character varying NOT NULL,
-    value character varying NOT NULL
-);
-
-
-ALTER TABLE public.client_settings OWNER TO canidae;
-
---
 -- Name: file; Type: TABLE; Schema: public; Owner: canidae; Tablespace: 
 --
 
 CREATE TABLE file (
     file_id integer NOT NULL,
-    client_id integer NOT NULL,
     track_id integer,
     filename character varying NOT NULL,
     last_updated timestamp without time zone DEFAULT now() NOT NULL,
@@ -105,6 +79,35 @@ CREATE TABLE metadata (
 
 
 ALTER TABLE public.metadata OWNER TO canidae;
+
+--
+-- Name: setting; Type: TABLE; Schema: public; Owner: canidae; Tablespace: 
+--
+
+CREATE TABLE setting (
+    setting_id integer NOT NULL,
+    setting_class_id integer NOT NULL,
+    key character varying NOT NULL,
+    value character varying NOT NULL,
+    user_changed boolean DEFAULT false NOT NULL,
+    description character varying NOT NULL
+);
+
+
+ALTER TABLE public.setting OWNER TO canidae;
+
+--
+-- Name: setting_class; Type: TABLE; Schema: public; Owner: canidae; Tablespace: 
+--
+
+CREATE TABLE setting_class (
+    setting_class_id integer NOT NULL,
+    name character varying NOT NULL,
+    description character varying NOT NULL
+);
+
+
+ALTER TABLE public.setting_class OWNER TO canidae;
 
 --
 -- Name: track; Type: TABLE; Schema: public; Owner: canidae; Tablespace: 
@@ -179,34 +182,6 @@ SELECT pg_catalog.setval('artist_artist_id_seq', 1, false);
 
 
 --
--- Name: client_client_id_seq; Type: SEQUENCE; Schema: public; Owner: canidae
---
-
-CREATE SEQUENCE client_client_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MAXVALUE
-    NO MINVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.client_client_id_seq OWNER TO canidae;
-
---
--- Name: client_client_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: canidae
---
-
-ALTER SEQUENCE client_client_id_seq OWNED BY client.client_id;
-
-
---
--- Name: client_client_id_seq; Type: SEQUENCE SET; Schema: public; Owner: canidae
---
-
-SELECT pg_catalog.setval('client_client_id_seq', 1, false);
-
-
---
 -- Name: file_file_id_seq; Type: SEQUENCE; Schema: public; Owner: canidae
 --
 
@@ -263,6 +238,62 @@ SELECT pg_catalog.setval('metadata_metadata_id_seq', 1, false);
 
 
 --
+-- Name: setting_class_setting_class_id_seq; Type: SEQUENCE; Schema: public; Owner: canidae
+--
+
+CREATE SEQUENCE setting_class_setting_class_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.setting_class_setting_class_id_seq OWNER TO canidae;
+
+--
+-- Name: setting_class_setting_class_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: canidae
+--
+
+ALTER SEQUENCE setting_class_setting_class_id_seq OWNED BY setting_class.setting_class_id;
+
+
+--
+-- Name: setting_class_setting_class_id_seq; Type: SEQUENCE SET; Schema: public; Owner: canidae
+--
+
+SELECT pg_catalog.setval('setting_class_setting_class_id_seq', 1, false);
+
+
+--
+-- Name: setting_setting_id_seq; Type: SEQUENCE; Schema: public; Owner: canidae
+--
+
+CREATE SEQUENCE setting_setting_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.setting_setting_id_seq OWNER TO canidae;
+
+--
+-- Name: setting_setting_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: canidae
+--
+
+ALTER SEQUENCE setting_setting_id_seq OWNED BY setting.setting_id;
+
+
+--
+-- Name: setting_setting_id_seq; Type: SEQUENCE SET; Schema: public; Owner: canidae
+--
+
+SELECT pg_catalog.setval('setting_setting_id_seq', 1, false);
+
+
+--
 -- Name: track_track_id_seq; Type: SEQUENCE; Schema: public; Owner: canidae
 --
 
@@ -305,13 +336,6 @@ ALTER TABLE artist ALTER COLUMN artist_id SET DEFAULT nextval('artist_artist_id_
 
 
 --
--- Name: client_id; Type: DEFAULT; Schema: public; Owner: canidae
---
-
-ALTER TABLE client ALTER COLUMN client_id SET DEFAULT nextval('client_client_id_seq'::regclass);
-
-
---
 -- Name: file_id; Type: DEFAULT; Schema: public; Owner: canidae
 --
 
@@ -326,6 +350,20 @@ ALTER TABLE metadata ALTER COLUMN metadata_id SET DEFAULT nextval('metadata_meta
 
 
 --
+-- Name: setting_id; Type: DEFAULT; Schema: public; Owner: canidae
+--
+
+ALTER TABLE setting ALTER COLUMN setting_id SET DEFAULT nextval('setting_setting_id_seq'::regclass);
+
+
+--
+-- Name: setting_class_id; Type: DEFAULT; Schema: public; Owner: canidae
+--
+
+ALTER TABLE setting_class ALTER COLUMN setting_class_id SET DEFAULT nextval('setting_class_setting_class_id_seq'::regclass);
+
+
+--
 -- Name: track_id; Type: DEFAULT; Schema: public; Owner: canidae
 --
 
@@ -336,7 +374,7 @@ ALTER TABLE track ALTER COLUMN track_id SET DEFAULT nextval('track_track_id_seq'
 -- Data for Name: album; Type: TABLE DATA; Schema: public; Owner: canidae
 --
 
-COPY album (album_id, artist_id, album_mbid, type, title, released, asin) FROM stdin;
+COPY album (album_id, artist_id, album_mbid, type, title, released, asin, custom_artist) FROM stdin;
 \.
 
 
@@ -349,26 +387,10 @@ COPY artist (artist_id, artist_mbid, type, name, sortname) FROM stdin;
 
 
 --
--- Data for Name: client; Type: TABLE DATA; Schema: public; Owner: canidae
---
-
-COPY client (client_id, name, password) FROM stdin;
-\.
-
-
---
--- Data for Name: client_settings; Type: TABLE DATA; Schema: public; Owner: canidae
---
-
-COPY client_settings (client_id, key, value) FROM stdin;
-\.
-
-
---
 -- Data for Name: file; Type: TABLE DATA; Schema: public; Owner: canidae
 --
 
-COPY file (file_id, client_id, track_id, filename, last_updated, duration, channels, bitrate, sample_rate, puid) FROM stdin;
+COPY file (file_id, track_id, filename, last_updated, duration, channels, bitrate, sample_rate, puid) FROM stdin;
 \.
 
 
@@ -377,6 +399,22 @@ COPY file (file_id, client_id, track_id, filename, last_updated, duration, chann
 --
 
 COPY metadata (metadata_id, file_id, key, value) FROM stdin;
+\.
+
+
+--
+-- Data for Name: setting; Type: TABLE DATA; Schema: public; Owner: canidae
+--
+
+COPY setting (setting_id, setting_class_id, key, value, user_changed, description) FROM stdin;
+\.
+
+
+--
+-- Data for Name: setting_class; Type: TABLE DATA; Schema: public; Owner: canidae
+--
+
+COPY setting_class (setting_class_id, name, description) FROM stdin;
 \.
 
 
@@ -421,30 +459,6 @@ ALTER TABLE ONLY artist
 
 
 --
--- Name: client_name_key; Type: CONSTRAINT; Schema: public; Owner: canidae; Tablespace: 
---
-
-ALTER TABLE ONLY client
-    ADD CONSTRAINT client_name_key UNIQUE (name, password);
-
-
---
--- Name: client_pkey; Type: CONSTRAINT; Schema: public; Owner: canidae; Tablespace: 
---
-
-ALTER TABLE ONLY client
-    ADD CONSTRAINT client_pkey PRIMARY KEY (client_id);
-
-
---
--- Name: client_settings_pkey; Type: CONSTRAINT; Schema: public; Owner: canidae; Tablespace: 
---
-
-ALTER TABLE ONLY client_settings
-    ADD CONSTRAINT client_settings_pkey PRIMARY KEY (client_id, key);
-
-
---
 -- Name: file_filename_key; Type: CONSTRAINT; Schema: public; Owner: canidae; Tablespace: 
 --
 
@@ -466,6 +480,38 @@ ALTER TABLE ONLY file
 
 ALTER TABLE ONLY metadata
     ADD CONSTRAINT metadata_pkey PRIMARY KEY (metadata_id);
+
+
+--
+-- Name: setting_class_name_key; Type: CONSTRAINT; Schema: public; Owner: canidae; Tablespace: 
+--
+
+ALTER TABLE ONLY setting_class
+    ADD CONSTRAINT setting_class_name_key UNIQUE (name);
+
+
+--
+-- Name: setting_class_pkey; Type: CONSTRAINT; Schema: public; Owner: canidae; Tablespace: 
+--
+
+ALTER TABLE ONLY setting_class
+    ADD CONSTRAINT setting_class_pkey PRIMARY KEY (setting_class_id);
+
+
+--
+-- Name: setting_pkey; Type: CONSTRAINT; Schema: public; Owner: canidae; Tablespace: 
+--
+
+ALTER TABLE ONLY setting
+    ADD CONSTRAINT setting_pkey PRIMARY KEY (setting_id);
+
+
+--
+-- Name: setting_setting_class_id_key; Type: CONSTRAINT; Schema: public; Owner: canidae; Tablespace: 
+--
+
+ALTER TABLE ONLY setting
+    ADD CONSTRAINT setting_setting_class_id_key UNIQUE (setting_class_id, key);
 
 
 --
@@ -493,22 +539,6 @@ ALTER TABLE ONLY album
 
 
 --
--- Name: client_settings_client_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: canidae
---
-
-ALTER TABLE ONLY client_settings
-    ADD CONSTRAINT client_settings_client_id_fkey FOREIGN KEY (client_id) REFERENCES client(client_id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-
---
--- Name: file_client_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: canidae
---
-
-ALTER TABLE ONLY file
-    ADD CONSTRAINT file_client_id_fkey FOREIGN KEY (client_id) REFERENCES client(client_id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-
---
 -- Name: file_track_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: canidae
 --
 
@@ -522,6 +552,14 @@ ALTER TABLE ONLY file
 
 ALTER TABLE ONLY metadata
     ADD CONSTRAINT metadata_file_id_fkey FOREIGN KEY (file_id) REFERENCES file(file_id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: setting_setting_class_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: canidae
+--
+
+ALTER TABLE ONLY setting
+    ADD CONSTRAINT setting_setting_class_id_fkey FOREIGN KEY (setting_class_id) REFERENCES setting_class(setting_class_id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
 --
@@ -548,6 +586,146 @@ REVOKE ALL ON SCHEMA public FROM PUBLIC;
 REVOKE ALL ON SCHEMA public FROM postgres;
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- Name: album; Type: ACL; Schema: public; Owner: canidae
+--
+
+REVOKE ALL ON TABLE album FROM PUBLIC;
+REVOKE ALL ON TABLE album FROM canidae;
+GRANT ALL ON TABLE album TO canidae;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE album TO locutus;
+
+
+--
+-- Name: artist; Type: ACL; Schema: public; Owner: canidae
+--
+
+REVOKE ALL ON TABLE artist FROM PUBLIC;
+REVOKE ALL ON TABLE artist FROM canidae;
+GRANT ALL ON TABLE artist TO canidae;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE artist TO locutus;
+
+
+--
+-- Name: file; Type: ACL; Schema: public; Owner: canidae
+--
+
+REVOKE ALL ON TABLE file FROM PUBLIC;
+REVOKE ALL ON TABLE file FROM canidae;
+GRANT ALL ON TABLE file TO canidae;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE file TO locutus;
+
+
+--
+-- Name: metadata; Type: ACL; Schema: public; Owner: canidae
+--
+
+REVOKE ALL ON TABLE metadata FROM PUBLIC;
+REVOKE ALL ON TABLE metadata FROM canidae;
+GRANT ALL ON TABLE metadata TO canidae;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE metadata TO locutus;
+
+
+--
+-- Name: setting; Type: ACL; Schema: public; Owner: canidae
+--
+
+REVOKE ALL ON TABLE setting FROM PUBLIC;
+REVOKE ALL ON TABLE setting FROM canidae;
+GRANT ALL ON TABLE setting TO canidae;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE setting TO locutus;
+
+
+--
+-- Name: setting_class; Type: ACL; Schema: public; Owner: canidae
+--
+
+REVOKE ALL ON TABLE setting_class FROM PUBLIC;
+REVOKE ALL ON TABLE setting_class FROM canidae;
+GRANT ALL ON TABLE setting_class TO canidae;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE setting_class TO locutus;
+
+
+--
+-- Name: track; Type: ACL; Schema: public; Owner: canidae
+--
+
+REVOKE ALL ON TABLE track FROM PUBLIC;
+REVOKE ALL ON TABLE track FROM canidae;
+GRANT ALL ON TABLE track TO canidae;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE track TO locutus;
+
+
+--
+-- Name: album_album_id_seq; Type: ACL; Schema: public; Owner: canidae
+--
+
+REVOKE ALL ON SEQUENCE album_album_id_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE album_album_id_seq FROM canidae;
+GRANT ALL ON SEQUENCE album_album_id_seq TO canidae;
+GRANT SELECT,UPDATE ON SEQUENCE album_album_id_seq TO locutus;
+
+
+--
+-- Name: artist_artist_id_seq; Type: ACL; Schema: public; Owner: canidae
+--
+
+REVOKE ALL ON SEQUENCE artist_artist_id_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE artist_artist_id_seq FROM canidae;
+GRANT ALL ON SEQUENCE artist_artist_id_seq TO canidae;
+GRANT SELECT,UPDATE ON SEQUENCE artist_artist_id_seq TO locutus;
+
+
+--
+-- Name: file_file_id_seq; Type: ACL; Schema: public; Owner: canidae
+--
+
+REVOKE ALL ON SEQUENCE file_file_id_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE file_file_id_seq FROM canidae;
+GRANT ALL ON SEQUENCE file_file_id_seq TO canidae;
+GRANT SELECT,UPDATE ON SEQUENCE file_file_id_seq TO locutus;
+
+
+--
+-- Name: metadata_metadata_id_seq; Type: ACL; Schema: public; Owner: canidae
+--
+
+REVOKE ALL ON SEQUENCE metadata_metadata_id_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE metadata_metadata_id_seq FROM canidae;
+GRANT ALL ON SEQUENCE metadata_metadata_id_seq TO canidae;
+GRANT SELECT,UPDATE ON SEQUENCE metadata_metadata_id_seq TO locutus;
+
+
+--
+-- Name: setting_class_setting_class_id_seq; Type: ACL; Schema: public; Owner: canidae
+--
+
+REVOKE ALL ON SEQUENCE setting_class_setting_class_id_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE setting_class_setting_class_id_seq FROM canidae;
+GRANT ALL ON SEQUENCE setting_class_setting_class_id_seq TO canidae;
+GRANT SELECT,UPDATE ON SEQUENCE setting_class_setting_class_id_seq TO locutus;
+
+
+--
+-- Name: setting_setting_id_seq; Type: ACL; Schema: public; Owner: canidae
+--
+
+REVOKE ALL ON SEQUENCE setting_setting_id_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE setting_setting_id_seq FROM canidae;
+GRANT ALL ON SEQUENCE setting_setting_id_seq TO canidae;
+GRANT SELECT,UPDATE ON SEQUENCE setting_setting_id_seq TO locutus;
+
+
+--
+-- Name: track_track_id_seq; Type: ACL; Schema: public; Owner: canidae
+--
+
+REVOKE ALL ON SEQUENCE track_track_id_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE track_track_id_seq FROM canidae;
+GRANT ALL ON SEQUENCE track_track_id_seq TO canidae;
+GRANT SELECT,UPDATE ON SEQUENCE track_track_id_seq TO locutus;
 
 
 --
