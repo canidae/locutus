77a8749b5570ac101f433329fe8277fac452ef85
Vidar Wahlberg <canidae@exent.net>
2008-09-01 22:36:59 +0200

    added config.pl and config.tmpl

---
+++ web/config.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+use lib '../include';
+use Locutus;
+
+my $page = 'config';
+my %vars = ();
+
+my $dbh = Locutus::db_connect();
+
+$vars{'settings'} = $dbh->selectall_arrayref('SELECT * FROM setting', {Slice => {}});
+
+#print Dumper(\%vars);
+
+Locutus::process_template($page, \%vars);
