dc029084503245f6c8ede591a6def30d286f667a
Vidar Wahlberg <canidae@exent.net>
2008-09-01 23:12:49 +0200

    changed default path of "sorted", "unsorted" and "duplicates" music

--- daemon/FileReader.h
+++ daemon/FileReader.h
@@ -3,13 +3,13 @@
 #define FILEREADER_H
 /* settings */
 #define MUSIC_OUTPUT_KEY "output_directory"
-#define MUSIC_OUTPUT_VALUE "/music/sorted/"
+#define MUSIC_OUTPUT_VALUE "/media/music/sorted/"
 #define MUSIC_OUTPUT_DESCRIPTION "Output directory"
 #define MUSIC_INPUT_KEY "input_directory"
-#define MUSIC_INPUT_VALUE "/music/unsorted/"
+#define MUSIC_INPUT_VALUE "/media/music/unsorted/"
 #define MUSIC_INPUT_DESCRIPTION "Input directory"
 #define MUSIC_DUPLICATE_KEY "duplicate_directory"
-#define MUSIC_DUPLICATE_VALUE "/music/duplicates/"
+#define MUSIC_DUPLICATE_VALUE "/media/music/duplicates/"
 #define MUSIC_DUPLICATE_DESCRIPTION "Directory for duplicate files"
 
 /* forward declare */
