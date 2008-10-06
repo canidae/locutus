a5d0a6731a0673eeb43a3c6d231660c3f03d5341
Vidar Wahlberg <canidae@exent.net>
2008-10-06 17:31:46 +0200

    changed default file_format. minor changes to web too

--- daemon/FileNamer.h
+++ daemon/FileNamer.h
@@ -17,7 +17,7 @@
 #define TYPE_DATE 12
 #define TYPE_CUSTOM_ARTIST 13 // custom_artist_sortname, albumartistsort (unless va), artistsort
 #define FILENAME_FORMAT_KEY "filename_format"
-#define FILENAME_FORMAT_VALUE "%custom_artist%/%album% - %tracknumber% - %artist% - %title%"
+#define FILENAME_FORMAT_VALUE "%albumartist%/%album%/%tracknumber% - %artist% - %title%"
 #define FILENAME_FORMAT_DESCRIPTION "Output filename format. Available keys: %album%, %albumartist%, %albumartistsort%, %artist%, %artistsort%, %musicbrainz_albumartistid%, %musicbrainz_albumid%, %musicbrainz_artistid%, %musicbrainz_trackid%, %musicip_puid%, %title%, %tracknumber%, %date%, %custom_artist%."
 
 #include <map>
