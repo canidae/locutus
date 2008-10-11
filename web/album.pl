a31adbf6e4c5c518fc82f97420308e543de303fe
Vidar Wahlberg <canidae@exent.net>
2008-10-11 15:22:44 +0200

    fixed typos, made track listing in album ordered correctly

--- web/album.pl
+++ web/album.pl
@@ -16,7 +16,7 @@ my $dbh = Locutus::db_connect();
 my $alid = int(param('alid'));
 
 $vars{'album'} = $dbh->selectrow_hashref('SELECT * FROM v_web_info_album WHERE album_id = ' . $alid);
-$vars{'tracks'} = $dbh->selectall_arrayref('SELECT * FROM v_web_list_tracks WHERE album_id = ' . $alid, {Slice => {}});
+$vars{'tracks'} = $dbh->selectall_arrayref('SELECT * FROM v_web_list_tracks WHERE album_id = ' . $alid . ' ORDER BY tracknumber ASC', {Slice => {}});
 
 #print Dumper(\%vars);
 
