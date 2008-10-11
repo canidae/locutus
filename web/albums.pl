05d93d8c682c3b4747c4b8c508f679fbfeb5bf12
Vidar Wahlberg <canidae@exent.net>
2008-10-11 14:01:55 +0200

    fixed albums.tmpl & albums.pl

--- web/albums.pl
+++ web/albums.pl
@@ -12,9 +12,7 @@ my %vars = ();
 
 my $dbh = Locutus::db_connect();
 
-$vars{'album'} = $dbh->selectrow_hashref('SELECT * FROM v_album_artist WHERE album_id = 1499');
-$vars{'tracks'} = $dbh->selectall_arrayref('SELECT * FROM v_track_match_file WHERE album_id = 1499', {Slice => {}});
-$vars{'prev_track'} = -1;
+$vars{'albums'} = $dbh->selectall_arrayref('SELECT * FROM v_web_list_albums ORDER BY title LIMIT 25', {Slice => {}});
 
 #print Dumper(\%vars);
 
