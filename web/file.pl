5f6acc16de64988678f6beb4c4220ff3574d9cf5
Vidar Wahlberg <canidae@exent.net>
2008-10-12 15:23:21 +0200

    new db-dump. made the file list show matching tracks

--- web/file.pl
+++ web/file.pl
@@ -16,7 +16,7 @@ my $dbh = Locutus::db_connect();
 my $fiid = int(param('fiid'));
 
 $vars{'file'} = $dbh->selectrow_hashref('SELECT * FROM v_web_info_file WHERE file_id = ' . $fiid);
-#$vars{'files'} = $dbh->selectall_arrayref('SELECT * FROM v_web_list_tracks WHERE album_id = ' . $trid . ' ORDER BY tracknumber ASC', {Slice => {}});
+$vars{'matches'} = $dbh->selectall_arrayref('SELECT * FROM v_web_list_matches WHERE file_file_id = ' . $fiid . ' ORDER BY mbid_match DESC, puid_match DESC, meta_score DESC', {Slice => {}});
 
 #print Dumper(\%vars);
 
