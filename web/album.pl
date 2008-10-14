b0a1f7109b556a6e6ebb8817ea2bf01fc0d2400e
Vidar Wahlberg <canidae@exent.net>
2008-10-15 00:46:47 +0200

    listing matching files to tracks when viewing a specific album

--- web/album.pl
+++ web/album.pl
@@ -15,9 +15,13 @@ my $dbh = Locutus::db_connect();
 
 my $alid = int(param('alid'));
 
-$vars{'album'} = $dbh->selectrow_hashref('SELECT * FROM v_web_info_album WHERE album_id = ' . $alid);
-$vars{'tracks'} = $dbh->selectall_arrayref('SELECT * FROM v_web_list_tracks WHERE album_id = ' . $alid . ' ORDER BY tracknumber ASC', {Slice => {}});
+$vars{album} = $dbh->selectrow_hashref('SELECT * FROM v_web_info_album WHERE album_id = ' . $alid);
+$vars{tracks} = $dbh->selectall_arrayref('SELECT * FROM v_web_list_tracks WHERE album_id = ' . $alid . ' ORDER BY tracknumber ASC', {Slice => {}});
 
-#print Dumper(\%vars);
+foreach my $track (@{$vars{tracks}}) {
+	$track->{files} = $dbh->selectall_arrayref('SELECT * FROM v_web_list_matches WHERE metatrack_track_mbid = ' . $dbh->quote($track->{mbid}) . ' ORDER BY mbid_match DESC, puid_match DESC, meta_score DESC', {Slice => {}});
+}
 
 Locutus::process_template($page, \%vars);
+
+#print Dumper(\%vars);
