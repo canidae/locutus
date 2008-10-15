c83cfdc6bb76b7b0fc000466c2342aabb961d47d
Vidar Wahlberg <canidae@exent.net>
2008-10-16 00:10:08 +0200

    just added some colors. everybody loves colors!

--- web/album.pl
+++ web/album.pl
@@ -13,13 +13,20 @@ my %vars = ();
 
 my $dbh = Locutus::db_connect();
 
-my $alid = int(param('alid'));
+my $alid = int(param('alid') || 0);
 
 $vars{album} = $dbh->selectrow_hashref('SELECT * FROM v_web_info_album WHERE album_id = ' . $alid);
 $vars{tracks} = $dbh->selectall_arrayref('SELECT * FROM v_web_list_tracks WHERE album_id = ' . $alid . ' ORDER BY tracknumber ASC', {Slice => {}});
 
 foreach my $track (@{$vars{tracks}}) {
 	$track->{files} = $dbh->selectall_arrayref('SELECT * FROM v_web_list_matches WHERE metatrack_track_mbid = ' . $dbh->quote($track->{mbid}) . ' ORDER BY mbid_match DESC, puid_match DESC, meta_score DESC', {Slice => {}});
+	foreach my $file (@{$track->{files}}) {
+		if ($file->{meta_score} > 0.5) {
+			$file->{color} = sprintf("%02x%02x00", (1.0 - ($file->{meta_score} - 0.5) * 2) * 255, 255);
+		} else {
+			$file->{color} = sprintf("%02x%02x00", 255, ($file->{meta_score} * 2) * 255);
+		}
+	}
 }
 
 Locutus::process_template($page, \%vars);
