7b6dcffd190e90c7e4e806b95c17556ec41591a5
Vidar Wahlberg <canidae@exent.net>
2008-11-03 20:04:55 +0100

    added pagination for files and fixed some minor bugs in the paginator

--- web/files.pl
+++ web/files.pl
@@ -3,26 +3,30 @@ use strict;
 use warnings;
 
 use CGI qw(:standard);
+use Data::Dumper;
 
 use lib '../include';
 use Locutus;
 
+my $dbh = Locutus::db_connect();
+my $limit = 25;
 my $page = 'files';
 my %vars = ();
 
-my $dbh = Locutus::db_connect();
-
-my $offset = int(param('offset'));
+my $offset = (int(param('page')) - 1) * $limit;
 $offset = 0 if ($offset < 0);
-my $only_unmatched = int(param('list_unmatched_only') || 0);
-$vars{'unmatched'} = $only_unmatched;
+
 my $filter = param('filter') || '';
+$filter = $dbh->quote('%' . $filter . '%');
 
-my $query = 'SELECT * FROM v_web_list_files WHERE filename ILIKE ' . $dbh->quote('%' . $filter . '%');
-# TODO: status (list_unmatched_only)
-#$query = $query . ' AND (no_album_match = true OR group_not_matched = true OR too_low_score = true)' if ($only_unmatched == 1);
-$query = $query . ' ORDER BY filename LIMIT 25 OFFSET ' . $offset;
-
+my $query = 'SELECT * FROM v_web_list_files WHERE filename ILIKE ' . $filter;
+$query = $query . ' ORDER BY filename LIMIT ' . $limit . ' OFFSET ' . $offset;
 $vars{'files'} = $dbh->selectall_arrayref($query, {Slice => {}});
+
+$query = 'SELECT count(*) FROM v_web_list_files WHERE filename ILIKE ' . $filter;
+my $count = int($dbh->selectrow_array($query));
+$vars{'pagination'} = Locutus::paginate($count, $offset, $limit);
+
+#print Dumper(\%vars);
 
 Locutus::process_template($page, \%vars);
