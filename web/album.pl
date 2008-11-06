519dc40962ec030c4ef85634f5ed93ac947c4421
Vidar Wahlberg <canidae@exent.net>
2008-11-07 00:47:54 +0100

    wuahahaha, bow down puny humans!
    i suspect vim wants me to write something short on the first line of the
    commit message, not have any text on the second line and write more in
    depth what i've done on the third line and below. at least it's putting
    weird colors all over the text if i don't. well, vim is probably right.
    oh yeah, albums now only list the same file once. "album matching" is
    still listing wrong (says 14/14 tracks matched even though it's only 5
    matching tracks), but i'll have to fix that another night

--- web/album.pl
+++ web/album.pl
@@ -18,11 +18,13 @@ my $alid = int(param('alid') || -1);
 $vars{album} = $dbh->selectrow_hashref('SELECT * FROM v_web_info_album WHERE album_id = ' . $alid);
 $vars{tracks} = $dbh->selectall_arrayref('SELECT * FROM v_web_list_tracks WHERE album_id = ' . $alid . ' ORDER BY tracknumber ASC', {Slice => {}});
 
+my $matches = $dbh->selectall_hashref('SELECT DISTINCT ON (file_id) * FROM v_web_track_list_matching_files WHERE track_id IN (SELECT track_id FROM track WHERE album_id = ' . $alid . ') ORDER BY file_id, mbid_match DESC, meta_score DESC', [qw(track_id file_id)]);
+
 foreach my $track (@{$vars{tracks}}) {
-	$track->{matches} = $dbh->selectall_arrayref('SELECT * FROM v_web_track_list_matching_files WHERE track_id = ' . $dbh->quote($track->{track_id}) . ' ORDER BY mbid_match DESC, puid_match DESC, meta_score DESC LIMIT 1', {Slice => {}});
-	foreach my $match (@{$track->{matches}}) {
+	while ((my $file, my $match) = each (%{$matches->{$track->{track_id}}})) {
 		$match->{color} = Locutus::score_to_color($match->{meta_score});
 		$match->{meta_score} = sprintf("%.1f%%", $match->{meta_score} * 100);
+		push (@{$track->{matches}}, $match);
 	}
 }
 
