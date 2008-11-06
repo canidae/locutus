d69fabcd70e69c9715512b44dd7e75ab48205d92
Vidar Wahlberg <canidae@exent.net>
2008-11-06 22:30:40 +0100

    forgot to fix listing matching files in album view

--- web/album.pl
+++ web/album.pl
@@ -19,7 +19,7 @@ $vars{album} = $dbh->selectrow_hashref('SELECT * FROM v_web_info_album WHERE alb
 $vars{tracks} = $dbh->selectall_arrayref('SELECT * FROM v_web_list_tracks WHERE album_id = ' . $alid . ' ORDER BY tracknumber ASC', {Slice => {}});
 
 foreach my $track (@{$vars{tracks}}) {
-	$track->{matches} = $dbh->selectall_arrayref('SELECT * FROM v_web_list_matches WHERE metatrack_track_mbid = ' . $dbh->quote($track->{mbid}) . ' ORDER BY mbid_match DESC, puid_match DESC, meta_score DESC', {Slice => {}});
+	$track->{matches} = $dbh->selectall_arrayref('SELECT * FROM v_web_track_list_matching_files WHERE track_id = ' . $dbh->quote($track->{track_id}) . ' ORDER BY mbid_match DESC, puid_match DESC, meta_score DESC', {Slice => {}});
 	foreach my $match (@{$track->{matches}}) {
 		$match->{color} = Locutus::score_to_color($match->{meta_score});
 		$match->{meta_score} = sprintf("%.1f%%", $match->{meta_score} * 100);
