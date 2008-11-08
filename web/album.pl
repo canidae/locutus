7e1841beca9739c8087beec6d257a775e5a53113
Vidar Wahlberg <canidae@exent.net>
2008-11-08 17:34:26 +0100

    fixed order of matching files in album view
    forgot to sort matching files after mbid_match & meta_score, so it
    listed the matching files in a random order

--- web/album.pl
+++ web/album.pl
@@ -16,7 +16,7 @@ my $dbh = Locutus::db_connect();
 my $alid = int(param('alid') || -1);
 
 $vars{album} = $dbh->selectrow_hashref('SELECT * FROM v_web_info_album WHERE album_id = ' . $alid);
-$vars{tracks} = $dbh->selectall_arrayref('SELECT * FROM v_web_album_list_tracks_and_matching_files WHERE album_id = ' . $alid . ' ORDER BY tracknumber ASC', {Slice => {}});
+$vars{tracks} = $dbh->selectall_arrayref('SELECT * FROM v_web_album_list_tracks_and_matching_files WHERE album_id = ' . $alid . ' ORDER BY tracknumber ASC, mbid_match DESC, meta_score DESC', {Slice => {}});
 
 foreach my $track (@{$vars{tracks}}) {
 	$track->{color} = Locutus::score_to_color($track->{meta_score});
