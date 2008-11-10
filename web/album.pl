1fb438fafcff700245054d7dedce9b04f8a9337b
Vidar Wahlberg <canidae@exent.net>
2008-11-10 19:54:05 +0100

    made it possible to match & force save files
    in the album view you can now match files with a track and "force save"
    the file. not really userfriendly, but this actually means it's possible
    to use locutus for sorting an archive. how cool is that?

--- web/album.pl
+++ web/album.pl
@@ -15,6 +15,22 @@ my $dbh = Locutus::db_connect();
 
 my $alid = int(param('alid') || -1);
 
+my @match_file_track = param('match_file_track');
+if (defined @match_file_track) {
+	my $force_save = param('save_selected');
+	foreach my $value (@match_file_track) {
+		my ($file_id, $track_id) = split (/@/, $value);
+		$file_id = int($file_id);
+		$track_id = int($track_id);
+		if ($file_id > 0 && $track_id > 0) {
+			my $query = 'UPDATE file SET user_matched = ' . $track_id;
+			$query .= ', force_save = true' if (defined $force_save);
+			$query .= ' WHERE file_id = ' . $file_id;
+			$dbh->do($query);
+		}
+	}
+}
+
 $vars{album} = $dbh->selectrow_hashref('SELECT * FROM v_web_info_album WHERE album_id = ' . $alid);
 $vars{tracks} = $dbh->selectall_arrayref('SELECT * FROM v_web_album_list_tracks_and_matching_files WHERE album_id = ' . $alid . ' ORDER BY tracknumber ASC, mbid_match DESC, meta_score DESC', {Slice => {}});
 
