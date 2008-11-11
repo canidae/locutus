a3770e95319ea3ccdb962c78588763d5fdd0f173
Vidar Wahlberg <canidae@exent.net>
2008-11-11 21:55:42 +0100

    added some data to the statistics page
    just for fun. text is fairly unreadable though, because i just had to
    set the color code to #ca9dae (no, you won't get a cookie if you figure
    out how i figured out that color code) ;)

--- web/statistics.pl
+++ web/statistics.pl
@@ -5,9 +5,15 @@ use warnings;
 use lib '../include';
 use Locutus;
 
+my $dbh = Locutus::db_connect();
 my $page = 'statistics';
 my %vars = ();
 
-my $dbh = Locutus::db_connect();
+$vars{daemon_stats} = $dbh->selectrow_hashref('SELECT active, start, stop, stop - start AS last_run_time, now() - start AS current_run_time FROM locutus');
+$vars{file_stats} = $dbh->selectrow_hashref('SELECT count(*) AS files, count(DISTINCT matched) AS matched_files, count(DISTINCT genre) AS genres, sum(duplicate::int) AS duplicates, min(duration) AS min_duration, max(duration) AS max_duration, sum(duration) AS total_duration, min(bitrate) AS min_bitrate, max(bitrate) AS max_bitrate, avg(bitrate) AS avg_bitrate FROM file');
+$vars{album_stats} = $dbh->selectrow_hashref('SELECT count(*) AS albums, count(DISTINCT artist_id) AS album_artists FROM album');
+$vars{track_stats} = $dbh->selectrow_hashref('SELECT count(*) AS tracks, count(DISTINCT artist_id) AS track_artists, min(duration) AS min_duration, max(duration) AS max_duration, max(tracknumber) AS max_tracknumber FROM track');
+$vars{match_stats} = $dbh->selectrow_hashref('SELECT count(*) AS matches, sum(mbid_match::int) AS mbid_matches, min(meta_score) AS min_score, max(meta_score) AS max_score, avg(meta_score) AS avg_score FROM match');
+
 
 Locutus::process_template($page, \%vars);
