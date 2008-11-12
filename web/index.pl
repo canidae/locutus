fba973231e73a41799d38c1fead0e2d111fd921d
Vidar Wahlberg <canidae@exent.net>
2008-11-12 18:02:38 +0100

    made index.pl a symlink to statistics.pl
    temporary solution

--- web/index.pl
+++ web/index.pl
@@ -1,13 +1,1 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-
-use lib '../include';
-use Locutus;
-
-my $page = 'statistics';
-my %vars = ();
-
-my $dbh = Locutus::db_connect();
-
-Locutus::process_template($page, \%vars);
+statistics.pl
