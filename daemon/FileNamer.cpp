de2d952ff42032c05e63909e30df6e49cb31afb2
Vidar Wahlberg <canidae@exent.net>
2008-12-14 02:08:29 +0100

    working on new filenamer
    current filenamer is quite limited. i'm working on one that handles
    functions as well as "variables" (stuff taken from metadata) and
    "static" (stuff that's neither functions nor variables, like "/")
    entries.
    not yet done with this, it's a large task (guess i should've made an own
    branch for this, but oh well, i'll just push to the "backup" repo for
    the time being)

--- daemon/FileNamer.cpp
+++ daemon/FileNamer.cpp
@@ -1,3 +1,4 @@
+#include <ostream>
 #include "Database.h"
 #include "Debug.h"
 #include "FileNamer.h"
@@ -7,27 +8,13 @@ using namespace std;
 
 /* constructors/destructor */
 FileNamer::FileNamer(Database *database) : database(database) {
-	format_mapping["%album%"] = TYPE_ALBUM;
-	format_mapping["%albumartist%"] = TYPE_ALBUMARTIST;
-	format_mapping["%albumartistsort%"] = TYPE_ALBUMARTISTSORT;
-	format_mapping["%artist%"] = TYPE_ARTIST;
-	format_mapping["%artistsort%"] = TYPE_ARTISTSORT;
-	format_mapping["%musicbrainz_albumartistid%"] = TYPE_MUSICBRAINZ_ALBUMARTISTID;
-	format_mapping["%musicbrainz_albumid%"] = TYPE_MUSICBRAINZ_ALBUMID;
-	format_mapping["%musicbrainz_artistid%"] = TYPE_MUSICBRAINZ_ARTISTID;
-	format_mapping["%musicbrainz_trackid%"] = TYPE_MUSICBRAINZ_TRACKID;
-	format_mapping["%musicip_puid%"] = TYPE_MUSICIP_PUID;
-	format_mapping["%title%"] = TYPE_TITLE;
-	format_mapping["%tracknumber%"] = TYPE_TRACKNUMBER;
-	format_mapping["%date%"] = TYPE_DATE;
-	format_mapping["%custom_artist%"] = TYPE_CUSTOM_ARTIST;
-	format_mapping["%genre%"] = TYPE_GENRE;
-
 	file_format = database->loadSettingString(FILENAME_FORMAT_KEY, FILENAME_FORMAT_VALUE, FILENAME_FORMAT_DESCRIPTION);
 	illegal_characters = database->loadSettingString(FILENAME_ILLEGAL_CHARACTERS_KEY, FILENAME_ILLEGAL_CHARACTERS_VALUE, FILENAME_ILLEGAL_CHARACTERS_DESCRIPTION);
 	string::size_type pos = illegal_characters.find('_', 0);
 	if (pos != string::npos)
 		illegal_characters.erase(pos, 1); // some idiot added "_" to illegal_characters
+
+	setupFields(0, file_format.size(), &fields);
 }
 
 FileNamer::~FileNamer() {
@@ -35,108 +22,10 @@ FileNamer::~FileNamer() {
 
 /* methods */
 const string &FileNamer::getFilename(Metafile *file) {
-	if (file_format.size() <= 0) {
-		filename.clear();
-		Debug::warning() << "File format for output is way too short, refuse to save file" << endl;
-		return filename;
-	}
-	string::size_type start = 0;
-	filename = file_format;
-	string::size_type stop = file->filename.find_last_of('.');
-	if (stop != string::npos)
-		filename.push_back('.'); // we need the "." before the extension (if any)
-	while (stop != string::npos && ++stop < file->filename.size()) {
-		if (file->filename[stop] >= 'A' && file->filename[stop] <= 'Z')
-			filename.push_back(file->filename[stop] + 32);
-		else
-			filename.push_back(file->filename[stop]);
-	}
-
-	while (start < filename.size() && (start = filename.find('%', start)) != string::npos) {
-		string::size_type stop = filename.find('%', start + 1);
-		if (stop == string::npos)
-			break; // no more '%'
-		map<string, int>::iterator replace = format_mapping.find(filename.substr(start, stop - start + 1)); // "+ 1" to include last '%'
-		if (replace != format_mapping.end()) {
-			/* we should replace something */
-			string tmp;
-			switch (replace->second) {
-				case TYPE_ALBUM:
-					tmp = file->album;
-					break;
-
-				case TYPE_ALBUMARTIST:
-					tmp = file->albumartist;
-					break;
-
-				case TYPE_ALBUMARTISTSORT:
-					tmp = file->albumartistsort;
-					break;
-
-				case TYPE_ARTIST:
-					tmp = file->artist;
-					break;
-
-				case TYPE_ARTISTSORT:
-					tmp = file->artistsort;
-					break;
-
-				case TYPE_MUSICBRAINZ_ALBUMARTISTID:
-					tmp = file->musicbrainz_albumartistid;
-					break;
-
-				case TYPE_MUSICBRAINZ_ALBUMID:
-					tmp = file->musicbrainz_albumid;
-					break;
-
-				case TYPE_MUSICBRAINZ_ARTISTID:
-					tmp = file->musicbrainz_artistid;
-					break;
-
-				case TYPE_MUSICBRAINZ_TRACKID:
-					tmp = file->musicbrainz_trackid;
-					break;
-
-				case TYPE_MUSICIP_PUID:
-					tmp = file->puid;
-					break;
-
-				case TYPE_TITLE:
-					tmp = file->title;
-					break;
-
-				case TYPE_TRACKNUMBER:
-					tmp = "";
-					if (atoi(file->tracknumber.c_str()) <= 9)
-						tmp.push_back('0');
-					tmp.append(file->tracknumber);
-					break;
-
-				case TYPE_DATE:
-					tmp = file->released;
-					break;
-
-				case TYPE_CUSTOM_ARTIST:
-					if (file->musicbrainz_artistid != VARIOUS_ARTISTS_MBID)
-						tmp = file->albumartistsort;
-					else
-						tmp = file->artistsort;
-					break;
-
-				case TYPE_GENRE:
-					tmp = file->genre;
-
-				default:
-					/* didn't match anything, probably static entry */
-					tmp = replace->first;
-					break;
-			}
-			convertIllegalCharacters(&tmp);
-			filename.erase(start, replace->first.size());
-			filename.insert(start, tmp);
-			start += tmp.size();
-		}
-	}
+	filename.clear();
+	for (vector<Field>::const_iterator f = fields.begin(); f != fields.end(); ++f)
+		filename.append(parseField(file, f));
+	convertIllegalCharacters(&filename);
 	return filename;
 }
 
@@ -145,4 +34,228 @@ void FileNamer::convertIllegalCharacters(string *text) {
 	string::size_type pos = 0;
 	while ((pos = text->find_first_of(illegal_characters), pos) != string::npos)
 		text->replace(pos, 1, "_");
+}
+
+const std::string &FileNamer::parseField(Metafile *file, const vector<Field>::const_iterator field) {
+	tmp_field.clear();
+	switch (field->type) {
+		/* static */
+		case TYPE_STATIC:
+			return field->data;
+
+		/* variables */
+		case TYPE_ALBUM:
+			return file->album;
+
+		case TYPE_ALBUMARTIST:
+			return file->albumartist;
+
+		case TYPE_ALBUMARTISTSORT:
+			return file->albumartistsort;
+
+		case TYPE_ARTIST:
+			return file->artist;
+
+		case TYPE_ARTISTSORT:
+			return file->artistsort;
+
+		case TYPE_MUSICBRAINZ_ALBUMARTISTID:
+			return file->musicbrainz_albumartistid;
+
+		case TYPE_MUSICBRAINZ_ALBUMID:
+			return file->musicbrainz_albumid;
+
+		case TYPE_MUSICBRAINZ_ARTISTID:
+			return file->musicbrainz_artistid;
+
+		case TYPE_MUSICBRAINZ_TRACKID:
+			return file->musicbrainz_trackid;
+
+		case TYPE_MUSICIP_PUID:
+			return file->puid;
+
+		case TYPE_TITLE:
+			return file->title;
+
+		case TYPE_TRACKNUMBER:
+			return file->tracknumber;
+
+		case TYPE_DATE:
+			return file->released;
+
+		case TYPE_GENRE:
+			return file->genre;
+
+		/* functions */
+		case TYPE_IF:
+			/* return field->fields[1] if field->fields[0] is not empty,
+			 * otherwise return field->fields[2] */
+			if (field->fields.size() == 3) {
+				vector<Field>::const_iterator f = field->fields.begin();
+				if (parseField(file, f++) != "")
+					return (parseField(file, f));
+				return (parseField(file, ++f));
+			}
+			break;
+
+		case TYPE_COALESCE:
+			/* return first non-empty field */
+			for (vector<Field>::const_iterator f = field->fields.begin(); f != field->fields.end(); ++f) {
+				if (parseField(file, f) != "")
+					return tmp_field;
+			}
+			break;
+
+		case TYPE_LOWER:
+			/* return data in lower case */
+			if (field->fields.size() == 1) {
+				ostringstream tmp;
+				tmp << nouppercase << parseField(file, field->fields.begin());
+				tmp_field = tmp.str();
+				return tmp_field;
+			}
+			break;
+
+		case TYPE_UPPER:
+			/* return data in upper case */
+			if (field->fields.size() == 1) {
+				ostringstream tmp;
+				tmp << uppercase << parseField(file, field->fields.begin());
+				tmp_field = tmp.str();
+				return tmp_field;
+			}
+			break;
+
+		/* error */
+		default:
+			Debug::warning() << "Field not implemented. Type: " << field->type << ", data: " << field->data << ", fields: " << field->fields.size() << endl;
+			break;
+	}
+	return tmp_field;
+}
+
+void FileNamer::removeEscapes(string *text) {
+	/* remove backslashes properly */
+	if (text == NULL || text->size() <= 0)
+		return;
+	string::size_type pos = -1;
+	while (pos - 1 < text->size() && (pos = text->find('\\', pos + 1)) != string::npos)
+		text->erase(pos, 1);
+}
+
+void FileNamer::setupFields(string::size_type start, string::size_type stop, vector<Field> *fields) {
+	/* setup fields for filename pattern */
+	string::size_type pos = start - 1;
+	string::size_type prev = start;
+	while ((pos = file_format.find_first_of("%$", pos + 1)) < stop && pos != string::npos) {
+		int backslashes = 0;
+		for (string::size_type a = pos - 1; a >= 0; --a) {
+			if (file_format[a] != '\\')
+				break;
+			++backslashes;
+		}
+		if (backslashes % 2 == 0)
+			continue; // this '%' or '$' is escaped, neither variable nor function
+		if (prev < pos) {
+			/* static field before variable/function */
+			Field f;
+			f.type = TYPE_STATIC;
+			f.data = file_format.substr(prev, pos - prev);
+			removeEscapes(&f.data);
+			fields->push_back(f);
+		}
+		if (file_format[pos] == '%') {
+			/* variable */
+			int type;
+			if (file_format.find("%musicbrainz_albumartistid%", pos) == 0)
+				type = TYPE_MUSICBRAINZ_ALBUMARTISTID;
+			else if (file_format.find("%musicbrainz_artistid%", pos) == 0)
+				type = TYPE_MUSICBRAINZ_ARTISTID;
+			else if (file_format.find("%musicbrainz_albumid%", pos) == 0)
+				type = TYPE_MUSICBRAINZ_ALBUMID;
+			else if (file_format.find("%musicbrainz_trackid%", pos) == 0)
+				type = TYPE_MUSICBRAINZ_TRACKID;
+			else if (file_format.find("%albumartistsort%", pos) == 0)
+				type = TYPE_ALBUMARTISTSORT;
+			else if (file_format.find("%musicip_puid%", pos) == 0)
+				type = TYPE_MUSICIP_PUID;
+			else if (file_format.find("%albumartist%", pos) == 0)
+				type = TYPE_ALBUMARTIST;
+			else if (file_format.find("%tracknumber%", pos) == 0)
+				type = TYPE_TRACKNUMBER;
+			else if (file_format.find("%artistsort%", pos) == 0)
+				type = TYPE_ARTISTSORT;
+			else if (file_format.find("%artist%", pos) == 0)
+				type = TYPE_ARTIST;
+			else if (file_format.find("%album%", pos) == 0)
+				type = TYPE_ALBUM;
+			else if (file_format.find("%genre%", pos) == 0)
+				type = TYPE_GENRE;
+			else if (file_format.find("%title%", pos) == 0)
+				type = TYPE_TITLE;
+			else if (file_format.find("%date%", pos) == 0)
+				type = TYPE_DATE;
+			else
+				continue; // not matching any variable, must be static entry
+			/* set pos to end '%' */
+			pos = file_format.find("%", pos + 1);
+			/* set prev to char after '%' */
+			prev = pos + 1;
+			/* add variable to field list */
+			Field f;
+			f.type = type;
+			f.data = "";
+			fields->push_back(f);
+		} else if (file_format[pos] == '$') {
+			/* function */
+			int type;
+			if (file_format.find("$coalesce(", pos) == 0)
+				type = TYPE_COALESCE;
+			else if (file_format.find("$lower(", pos) == 0)
+				type = TYPE_LOWER;
+			else if (file_format.find("$upper(", pos) == 0)
+				type = TYPE_UPPER;
+			else if (file_format.find("$if(", pos) == 0)
+				type = TYPE_IF;
+			else
+				continue; // not matching any function, must be static entry
+			/* find end parenthese */
+			string::size_type end = pos;
+			while ((end = file_format.find(")", end + 1)) != string::npos) {
+				int backslashes = 0;
+				for (string::size_type a = end - 1; a >= 0; --a) {
+					if (file_format[a] != '\\')
+						break;
+					++backslashes;
+				}
+				if (backslashes % 2 == 0)
+					break; // this parenthese is escaped, not the end parenthese
+			}
+			if (end == string::npos) {
+				/* no end parenthese?
+				 * user probably did something wrong or it's a static entry */
+				continue;
+			}
+			/* set up function */
+			Field f;
+			f.type = type;
+			f.data = "";
+			/* call this method recursively for fields within parentheses in this function */
+			setupFields(file_format.find("(", pos + 1) + 1, end - 1, &f.fields);
+			/* add function to field list */
+			fields->push_back(f);
+			/* set pos to end parenthese */
+			pos = end;
+			/* set prev to char after end parenthese */
+			prev = pos + 1;
+		}
+	}
+	if (prev < stop) {
+		/* static field after last variable/function */
+		Field f;
+		f.type = TYPE_STATIC;
+		f.data = file_format.substr(prev);
+		removeEscapes(&f.data);
+		fields->push_back(f);
+	}
 }
--- daemon/FileNamer.h
+++ daemon/FileNamer.h
@@ -1,31 +1,45 @@
 #ifndef FILENAMER_H
 #define FILENAMER_H
 #define VARIOUS_ARTISTS_MBID "89ad4ac3-39f7-470e-963a-56509c546377"
-/* file naming */
-#define TYPE_ALBUM 0
-#define TYPE_ALBUMARTIST 1
-#define TYPE_ALBUMARTISTSORT 2
-#define TYPE_ARTIST 3
-#define TYPE_ARTISTSORT 4
-#define TYPE_MUSICBRAINZ_ALBUMARTISTID 5
-#define TYPE_MUSICBRAINZ_ALBUMID 6
-#define TYPE_MUSICBRAINZ_ARTISTID 7
-#define TYPE_MUSICBRAINZ_TRACKID 8
-#define TYPE_MUSICIP_PUID 9
-#define TYPE_TITLE 10
-#define TYPE_TRACKNUMBER 11
-#define TYPE_DATE 12
-#define TYPE_CUSTOM_ARTIST 13 // albumartistsort (unless va), artistsort
-#define TYPE_GENRE 14
+/* settings */
 #define FILENAME_FORMAT_KEY "filename_format"
 #define FILENAME_FORMAT_VALUE "%albumartist%/%album%/%tracknumber% - %artist% - %title%"
 #define FILENAME_FORMAT_DESCRIPTION "Output filename format. Available keys: %album%, %albumartist%, %albumartistsort%, %artist%, %artistsort%, %musicbrainz_albumartistid%, %musicbrainz_albumid%, %musicbrainz_artistid%, %musicbrainz_trackid%, %musicip_puid%, %title%, %tracknumber%, %date%, %custom_artist%, %genre%."
 #define FILENAME_ILLEGAL_CHARACTERS_KEY "filename_illegal_characters"
 #define FILENAME_ILLEGAL_CHARACTERS_VALUE "/"
 #define FILENAME_ILLEGAL_CHARACTERS_DESCRIPTION "Characters in metadata that will be converted to '_' in filename."
+/* types */
+/* static */
+#define TYPE_STATIC 0
+/* variables */
+#define TYPE_ALBUM 1
+#define TYPE_ALBUMARTIST 2
+#define TYPE_ALBUMARTISTSORT 3
+#define TYPE_ARTIST 4
+#define TYPE_ARTISTSORT 5
+#define TYPE_MUSICBRAINZ_ALBUMARTISTID 6
+#define TYPE_MUSICBRAINZ_ALBUMID 7
+#define TYPE_MUSICBRAINZ_ARTISTID 8
+#define TYPE_MUSICBRAINZ_TRACKID 9
+#define TYPE_MUSICIP_PUID 10
+#define TYPE_TITLE 11
+#define TYPE_TRACKNUMBER 12
+#define TYPE_DATE 13
+#define TYPE_GENRE 14
+/* functions */
+#define TYPE_IF 101
+#define TYPE_COALESCE 102
+#define TYPE_LOWER 103
+#define TYPE_UPPER 104
 
-#include <map>
 #include <string>
+#include <vector>
+
+struct Field {
+	int type;
+	std::string data;
+	std::vector<Field> fields;
+};
 
 class Database;
 class Metafile;
@@ -39,11 +53,15 @@ class FileNamer {
 
 	private:
 		Database *database;
-		std::map<std::string, int> format_mapping;
 		std::string filename;
+		std::string tmp_field;
 		std::string file_format;
 		std::string illegal_characters;
+		std::vector<Field> fields;
 
 		void convertIllegalCharacters(std::string *text);
+		const std::string &parseField(Metafile *file, const std::vector<Field>::const_iterator field);
+		void removeEscapes(std::string *text);
+		void setupFields(std::string::size_type start, std::string::size_type stop, std::vector<Field> *fields);
 };
 #endif
