a7a7d902df06658da92e842ae3fcfb6a3c7a4581
Vidar Wahlberg <canidae@exent.net>
2008-12-20 13:21:33 +0100

    fix for user interface on bug #309853
    added a locutus.conf for the user interface as well. slightly hackish
    atm, but "it works"

--- include/Locutus.pm
+++ include/Locutus.pm
@@ -6,29 +6,28 @@ use Template;
 
 package Locutus;
 
+my %config;
+
+# read config
+read_config();
+
 sub db_connect {
-	my $dbh = DBI->connect("dbi:Pg:dbname=locutus;host=sql.samfundet.no", "locutus", "locutus") || die "Couldn't connect to database: " . DBI::errstr();
+	my $dbh = DBI->connect("dbi:Pg:dbname=" . $config{database_name} . ";host=" . $config{database_host}, $config{database_user}, $config{database_pass}) || die "Couldn't connect to database: " . DBI::errstr();
 	return $dbh;
 }
 
-sub process_template {
-	my ($page, $vars) = @_;
-
-	my $template = Template->new({
-			INCLUDE_PATH => '../templates'
-		});
-	print "Content-type: text/html\n\n";
-	$template->process($page . ".tmpl", $vars) || die "Template process failed: ", $template->error(), "\n";
-}
-
-sub score_to_color {
-	my $score = shift;
-	if ($score > 0.75) {
-		return sprintf("%02x%02x00", (1.0 - ($score - 0.75) * 4) * 255, 255);
-	} elsif ($score > 0.25) {
-		return sprintf("%02x%02x00", 255, (($score - 0.25) * 2) * 255);
+sub read_config {
+	open(CONFIG, "../include/locutus.conf") or die "Unable to open locutus.conf: $!";
+	while (<CONFIG>) {
+		chomp;
+		s/#.*//;
+		s/^\s+//;
+		s/\s+$//;
+		next unless length;
+		my ($setting, $value) = split(/\s*=\s*/, $_, 2);
+		$config{$setting} = $value;
 	}
-	return 'ff0000';
+	close(CONFIG);
 }
 
 sub paginate {
@@ -78,6 +77,26 @@ sub paginate {
 
 	$data{'data'} = \@pages;
 	return \%data;
+}
+
+sub process_template {
+	my ($page, $vars) = @_;
+
+	my $template = Template->new({
+			INCLUDE_PATH => '../templates'
+		});
+	print "Content-type: text/html\n\n";
+	$template->process($page . ".tmpl", $vars) || die "Template process failed: ", $template->error(), "\n";
+}
+
+sub score_to_color {
+	my $score = shift;
+	if ($score > 0.75) {
+		return sprintf("%02x%02x00", (1.0 - ($score - 0.75) * 4) * 255, 255);
+	} elsif ($score > 0.25) {
+		return sprintf("%02x%02x00", 255, (($score - 0.25) * 2) * 255);
+	}
+	return 'ff0000';
 }
 
 1;
