1bffe923edda1a44bf87f56fa7f987816aa6ecdb
Vidar Wahlberg <canidae@exent.net>
2009-02-08 14:51:22 +0100

    added license details to every file in daemon

--- daemon/FileNamer.h
+++ daemon/FileNamer.h
@@ -1,3 +1,16 @@
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+// 
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef FILENAMER_H
 #define FILENAMER_H
 #define VARIOUS_ARTISTS_MBID "89ad4ac3-39f7-470e-963a-56509c546377"
