83340621fb8c8bbeb0e8399d66f146108a1d6dff
Vidar <canidae@exent.net>
2009-05-05 00:44:35 +0200

    just added a virtual destructor to Database.h

--- daemon/Database.h
+++ daemon/Database.h
@@ -26,6 +26,8 @@ class Track;
 
 class Database {
 public:
+	virtual ~Database() {}
+
 	virtual bool init() = 0;
 	virtual bool loadAlbum(Album *album) = 0;
 	virtual const std::vector<Metafile *> &loadGroup(const std::string &group) = 0;
